a722273567ec53e7bd7b5cf99fb38ae29843a6eb
me <me@a.com>
2019-08-24 14:16:21 +0800

    api now accepts form-data that has no filename field

--- server/upload.js
+++ server/upload.js
@@ -5,7 +5,7 @@ module.exports = function upload(req, res) {
 
   const form = new IncomingForm();
 
-  let filename = '-';
+  let filename;
 
   // form.on('file', (field, file) => {
   //   // Do something with the file
@@ -16,13 +16,13 @@ module.exports = function upload(req, res) {
   //     .pipe(fs.createWriteStream('./uploads/' + file.name));
   // });
 
-  // gumana naman
   form.on('field', (name, value) => {
     filename = value;
   });
   form.on('file', (field, file) => {
-    if (filename === '-') {
+    if ( !filename ) {
       const generated = Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15) +
       Math.random().toString(36).substring(2, 15) + '.' +
       file.name.split('.').pop();
       filename = generated;
