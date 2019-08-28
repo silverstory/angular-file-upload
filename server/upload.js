3e8141df159a887ce822e65bd1cc507386a41e04
eprelofcdesktop <eprelofcdesktop@am.com>
2019-08-28 12:08:09 +0800

    Added file revision strategy

--- server/upload.js
+++ server/upload.js
@@ -19,23 +19,60 @@ module.exports = function upload(req, res) {
   form.on('field', (name, value) => {
     filename = value;
   });
-  form.on('file', (field, file) => {
-    if ( !filename ) {
+  form.on('file', async (field, file) => {
+    if (!filename) {
       const generated = Math.random().toString(36).substring(2, 15) +
-      Math.random().toString(36).substring(2, 15) +
-      Math.random().toString(36).substring(2, 15) + '.' +
-      file.name.split('.').pop();
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15) + '.' +
+        file.name.split('.').pop();
       filename = generated;
     }
     // Do something with the file
     // e.g. save it to the database
     // you can access it using file.path
-    console.log(file.name  + ' > ' + filename);
+
+    // add version literal to the filename
+    try {
+    if (fs.existsSync(`/uploads/${filename}`)) {
+
+      // file exists remove file extension
+      const x = filename.replace(/\.[^/.]+$/, "");
+      // // remove file extension
+      // const y = filename.replace(/\.[^/.]+$/, "");
+      // // get version number
+      // // wait filename doesn't have -v yet, should be filename from find files by regex
+      // const x = y.split('-v').pop();
+      // // increment version number
+      // let z = +x;
+      // z++;
+      // // take highest version number from loop
+      // let version = z.toString();
+      // if (isNaN(version)) {
+      //   version = 1;
+      // }
+
+      // generate new version digest
+      const v = Math.random().toString(36).substring(2, 15);
+      // rename file with new version
+      const oldPath = `/uploads/${filename}`;
+      const newPath = `/uploads/${x}-v${v}.${filename.split('.').pop()}`;
+      console.log(oldPath + ' > ' + newPath);
+      fs.rename(oldPath, newPath, (err) => {
+        // handle error in here
+        // console.error(err)
+      })
+    }
+    } catch (err) {
+      console.error(err);
+    }
+    console.log(file.name + ' > ' + filename);
     const readStream = fs.createReadStream(file.path)
       .pipe(fs.createWriteStream('/uploads/' + filename));
   });
   form.on('end', () => {
-    res.json( { path: `http://${ process.env.HOST }/${ filename }` } );
+    res.json({
+      path: `http://${ process.env.HOST }/${ filename }`
+    });
   });
   form.parse(req);
 };
