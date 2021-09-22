e7deae95fc863e588888452a07e785fc680f06dd
Sachu <april.medina2011@gmail.com>
2021-09-22 11:26:44 +0800

    basura:

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const upload = require('./upload');
-const hrms = require('./hrms')
+const hrms = require('./hrms');
+const locator = require('./hrms/locator');
+const accomplishment = require('./hrms/accomplishment');
+const photos = require('./hrms/photos');
 const sourcecode = require('./sourcecode');
 const cors = require('cors');
 
@@ -16,6 +19,12 @@ server.use(cors(corsOptions));
 server.post('/upload', upload);
 
 server.post('/hrms', hrms);
+
+server.post('/locator', locator);
+
+server.post('/accomplishment', accomplishment);
+
+server.post('/photos', photos);
 
 server.post('/sourcecode', sourcecode);
 
